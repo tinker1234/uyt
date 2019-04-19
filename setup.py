ba02b5f0c81653c1efa75ec3c4ab17b25c734024
tinker1234 <30789433+tinker1234@users.noreply.github.com>
2019-04-19 19:39:00 -0400

    Fixed email

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
 	 author="Tink Tink",
-	 author_email="TinkTink@11011gmail.com",
+	 author_email="TinkTink11011@gmail.com",
 	 description="Scrap YouTube",
 	 license="GNU GPLv3",
 	 keywords="youtube scrap api",
